b0a41d75e9032e68457cc886af6c010dcec5aba5
Sarim <sarimk80@gmail.com>
2024-04-16 21:54:26 +0500

    Initial Commit

---
+++ Create3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Create3D
+//
+//  Created by sarim khan on 16/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Create3D/Create3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Create3DApp.swift
+//  Create3D
+//
+//  Created by sarim khan on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Create3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
